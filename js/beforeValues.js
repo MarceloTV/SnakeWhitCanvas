eec8a4fa26d8ef6de16310757a86eb5fcd7e987e
Marcelino A. Abreu Mejia <marcelinomejia2009@gmail.com>
2021-01-09 21:29:39 -0400

    miss some little details

--- js/beforeValues.js
+++ js/beforeValues.js
@@ -2,6 +2,13 @@ class BeforeValues{
     vectors = []
 
     pushValues = (x,y) => {
+        if(this.vectors.length === 2000){
+            this.vectors.forEach((v,i) => {
+                if(i >= 1000){
+                    this.vectors.splice(i,1)
+                }
+            })
+        }
         this.vectors.unshift({
             x,
             y
--- js/index.js
+++ js/index.js
@@ -1,25 +1,55 @@
 import BeforeValues from './beforeValues.js'
 
 class Game{
-    constructor(){
+    constructor(btn){
+        this.score = 0
         this.run = {
             start: null,
             walk: null
         }
+        this.runVelocity = 5
         this.canvas = document.getElementById("canvas")
         this.ctx = this.canvas.getContext("2d")
         this.x = 10;
         this.y = 10;
         this.velX = 10;
         this.velY = 0;
-        this.ctx.width = this.canvas.width
-        this.ctx.height = this.canvas.height
         this.randomPoint = {
             x: null,
             y: null
         }
         this.history = []
         this.tail = []
+        this.light = document.getElementById("light")
+        this.btn = btn
+    }
+
+    reset = () => {
+        this.score = 0
+        this.run = {
+            start: null,
+            walk: null
+        }
+        this.x = 10;
+        this.y = 10;
+        this.velX = 10;
+        this.velY = 0;
+        this.randomPoint = {
+            x: null,
+            y: null
+        }
+        this.history = []
+        this.tail = []
+    }
+
+    scoreFunction = (ctx) => {
+        if(this.randomPoint.x == this.x && this.randomPoint.y == this.y){
+            this.score += 1
+            console.log(this.score)
+        }
+
+        ctx.font= "30px Helvetica"
+        ctx.fillText(this.score, 360,30)
     }
 
     testCanvas = (f) => {
@@ -118,30 +148,56 @@ class Game{
         
     }
 
+    gameOver = (ctx,canvas,score) => {
+        clearInterval(this.run.start)    
+        clearInterval(this.run.walk)
+
+        if(this.light.classList.contains("light_red")){
+            this.light.classList.remove("light_red")
+        }
+
+        this.ctx.clearRect(0,0,canvas.width,canvas.height)
+        ctx.beginPath()
+        ctx.font = "30px Helvetica"
+        ctx.fillStyle = "purple"
+        ctx.textAlign = "center"
+        ctx.fillText("Game Over", canvas.width/2, canvas.height/2)
+
+        ctx.beginPath()
+        ctx.font = "15px Helvetica"
+        ctx.fillStyle = "purple"
+        ctx.textAlign = "center"
+        ctx.fillText(`Your Score: ${score}`, canvas.width/2, (canvas.height/2) + 20)
+        this.reset()
+        this.btn.removeAttribute("disabled")
+    }
+
     checkColapse = () => {
         switch (this.x) {
             case 400:
-                alert("collapse")
-                clearInterval(this.run)    
+                this.gameOver(this.ctx,this.canvas,this.score)    
             break;
         
             case -10:
-                alert("collapse")
-                clearInterval(this.run)
+                this.gameOver(this.ctx,this.canvas,this.score)
             break;
         }
 
         switch (this.y) {
             case 400:
-                alert("collapse")
-                clearInterval(this.run)    
+                this.gameOver(this.ctx,this.canvas,this.score)    
             break;
         
             case -10:
-                alert("collapse")
-                clearInterval(this.run)
+                this.gameOver(this.ctx,this.canvas,this.score)
             break;
         }
+
+        this.tail.forEach((v,i) => {
+            if(this.history[i].x == this.x && this.history[i].y == this.y){
+                this.gameOver(this.ctx,this.canvas,this.score)
+            }
+        })
     }
 
     snake = (x,y) => {
@@ -152,18 +208,20 @@ class Game{
     animation = () => {
        this.run.start = setInterval(() => {
             this.ctx.clearRect(0,0,this.canvas.width,this.canvas.height)
+            this.scoreFunction(this.ctx)
             this.tailFunction(this.ctx)
             this.snake(this.x,this.y)
-            this.checkColapse()
             this.randomPointFunction(this.ctx)
+            this.checkColapse()
         },1000/60)
 
         this.run.walk = setInterval(() => {
+            this.light.classList.toggle("light_red")
             this.history = BeforeValues.getValues()
             BeforeValues.pushValues(this.x,this.y)
             this.x += this.velX
             this.y += this.velY
-        },1000/5)
+        },1000/this.runVelocity)
     }
 
     start = () => {
@@ -172,24 +230,14 @@ class Game{
     }
 }
 
-/* const btn = document.querySelector("#btn")
-const stop = document.querySelector("#stop")
-const test = document.querySelector("#test") */
+const btn = document.getElementById("start")
 
-const Main = new Game()
-
-/* stop.addEventListener('click', e => {
-    clearInterval(Main.run.start)
-    clearInterval(Main.run.walk)
-})
-
-test.addEventListener('click', e => {
-    console.log(Main.history)
-})
+const Main = new Game(btn)
 
 btn.addEventListener("click", e => {
     Main.start()
-}) */
+    e.currentTarget.disabled = true
+}) 
 
 window.addEventListener("keyup", e => {
     switch (e.key) {
